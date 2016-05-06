36caac671e2de5271fc352e7545da37545295db1
Martin Marianetti <martin.marianetti@gmail.com>
2016-05-06 16:42:31 -0300

    starting to code models

---
+++ src/Zoho/Crm/Models/Account.php
@@ -0,0 +1,13 @@
+<?php
+namespace Zoho\Crm\Models;
+
+class Account {
+
+protected $accountName;
+protected $accountOwner;
+protected $website;
+protected $tickerSymbol;
+protected $parentAccount;
+protected $employess;
+
+}
